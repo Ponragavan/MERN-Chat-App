c5eb3514d09708b3148a377624b9406c2fab2586
Ponragavan M <141037337+Ponragavan@users.noreply.github.com>
2024-10-11 09:38:12 +0530

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -52,6 +52,9 @@ exports.login = async (req, res) => {
 exports.getUser = async (req, res) => {
   try {
     const user = await User.findById(req.user.id).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
     return res.status(200).json(user);
   } catch (error) {
     return res.status(500).json({ message: error.message });
